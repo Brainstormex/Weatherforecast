0f227cc5b8c8cb3a4258a665e26eff9583c85700
uzxr <uzersayed9@gmail.com>
2023-03-04 20:54:12 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to bg-lime-400 h-[100vh] w-full'>
+      helo
+    </div>
+  )
+}
+
+export default App
